826138ee5e7b16bba873bfe04760cd37560517eb
kenshin <kenshin.acs@gmail.com>
2013-06-26 00:30:05 +0800

    supplement redis queue build-in  method
    Add Consume, Clear, Empty method etc...

--- redisq.go
+++ redisq.go
@@ -14,7 +14,7 @@ func (rq *RedisQueue) Put(msgs []string) error {
 	for _, m := range msgs {
 		err := rq.redis.Rpush(rq.name, []byte(m))
 		if err != nil {
-			log.Printf("insert '%s' into redis queue failed: %s\n", string(m), err)
+			log.Printf("[redis queue] put '%s' failed: %s\n", string(m), err)
 			return err
 		}
 	}
@@ -28,15 +28,54 @@ func (rq *RedisQueue) Get(block bool, timeout uint) (msg []byte, err error) {
 		msg, err = rq.redis.Lpop(rq.name)
 	}
 	if err != nil {
-		log.Printf("get message from redis queue failed: %s\n", err)
+		log.Printf("[redis queue] get message failed: %s\n", err)
 	}
-	log.Printf("get: %s\n", string(msg))
 	return
 }
 
-func (rq *RedisQueue) GetUnBlock() (msg []byte, err error) {
+func (rq *RedisQueue) GetNoWait() (msg []byte, err error) {
 	msg, err = rq.Get(false, 0)
 	return
+}
+
+func (rq *RedisQueue) Consume(block bool, timeout uint, msgs chan []byte) {
+	go func() {
+		for {
+			msg, err := rq.Get(block, timeout)
+			if err != nil {
+				log.Printf("[redis queue] consumer exit. since of: %s\n", err)
+				break
+			}
+			msgs <- msg
+		}
+	}()
+}
+
+func (rq *RedisQueue) Length() int {
+	len, err := rq.redis.Llen(rq.name)
+	if err != nil {
+		log.Printf("get redis queue length failed: %s\n", err)
+		return -1
+	}
+	return len
+}
+
+func (rq *RedisQueue) Empty() bool {
+	len, err := rq.redis.Llen(rq.name)
+	if err != nil {
+		log.Printf("get redis queue length failed: %s\n", err)
+		return true
+	}
+	return len == 0
+}
+
+func (rq *RedisQueue) Clear() error {
+	_, err := rq.redis.Del(rq.name)
+	if err != nil {
+		log.Printf("clear redis queue failed : %s\n", err)
+		return err
+	}
+	return nil
 }
 
 func (rq *RedisQueue) String() string {
