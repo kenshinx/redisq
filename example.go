85b22978e9b5e5c0cff6062c0ea0f5a65347c5ef
kenshin <kenshin.acs@gmail.com>
2013-06-26 00:35:22 +0800

    add example

---
+++ example.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"redisq"
+)
+
+const (
+	Server   = "127.0.1.1:6379"
+	Db       = 0
+	Password = ""
+	Name     = "redisq:kenshin"
+)
+
+var rq = redisq.NewRedisQueue(Server, Db, Password, Name)
+
+func put() {
+	mesg := []string{"a", "b", "c", "d"}
+	rq.Put(mesg)
+}
+
+func get() {
+	msg, _ := rq.Get(true, 1)
+	fmt.Printf("get: %s\n", msg)
+}
+
+func getNoWait() {
+	msg, err := rq.GetNoWait()
+	fmt.Printf("get: %s\n", msg)
+}
+
+func consume() {
+	var msgs = make(chan []byte)
+	rq.Consume(true, 1, msgs)
+	for {
+		msg := <-msgs
+		if msg != nil {
+			fmt.Printf("get msg: %s\n", string(msg))
+		}
+
+	}
+}
+
+func clear() {
+	rq.Clear()
+}
+
+func length() {
+	len := rq.Length()
+	fmt.Printf("queue length:%d \n", len)
+}
+
+func empty() {
+	isEmpty := rq.Empty()
+	fmt.Printf("queue is empty? :%t\n", isEmpty)
+}
+
+func main() {
+	get()
+}
