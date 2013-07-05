f71d21d93dee56f97fc3c6a0877762a16c496cdc
kenshin <kenshin.acs@gmail.com>
2013-07-05 14:28:05 +0800

    enhance consume example code.

--- README.md
+++ README.md
@@ -30,20 +30,34 @@ var rq = redisq.NewRedisQueue(Server, Db, Password, Name)
 #### Put
 
 ```
+type Foo struct {
+	X string
+	Y int
+}
+
 rq.Put("a")
 rq.Put(1)
+rq.Put(true)
+rq.Put(1.1)
 rq.Put([]string{"a", "b", "c"})
 rq.Put(map[string]int{"a": 1, "b": 2})
+
+f := Foo{"a", 1}
+rq.Put(f)
 ```
-* The raw data will be json encode,then insert into redis
+* Map type: map[int]string is unsupport since of json library
+* struct is supported imperfect now,will be transform into map, when pop out from redis
 
 query by redis-cli:
 
 	redis 127.0.0.1:6379> lrange redisq:kenshin 0 -1
 	1) "\"a\""
 	2) "1"
-	3) "[\"a\",\"b\",\"c\"]"
-	4) "{\"a\":1,\"b\":2}"
+	3) "true"
+	4) "1.1"
+	5) "[\"a\",\"b\",\"c\"]"
+	6) "{\"a\":1,\"b\":2}"
+	7) "{\"X\":\"a\",\"Y\":1}"
 
 	//json encoded data
 
--- example.go
+++ example.go
@@ -17,11 +17,24 @@ var rq = redisq.NewRedisQueue(Server, Db, Password, Name)
 
 func put() {
 
+	type Foo struct {
+		X string
+		Y int
+	}
+
 	rq.Put("a")
 	rq.Put(1)
+	rq.Put(true)
+	rq.Put(1.1)
 	rq.Put([]string{"a", "b", "c"})
 	rq.Put(map[string]int{"a": 1, "b": 2})
-	//json unsupported type: map[int]string
+	// json unsupported type: map[int]string
+
+	f := Foo{"a", 1}
+	rq.Put(f)
+
+	// struct is supported imperfect.
+	// strcut type will be transform into map, when pop out from redis.
 
 }
 
@@ -40,10 +53,40 @@ func consume() {
 	rq.Consume(true, 1, msgs)
 	for {
 		msg := <-msgs
-		if msg != nil {
-			fmt.Printf("get msg: %v,type:%s\n", msg, reflect.TypeOf(msg))
+		if msg == nil {
+			continue
 		}
-
+		fmt.Println(reflect.TypeOf(msg))
+		k := reflect.TypeOf(msg).Kind()
+		if k >= reflect.Int && k <= reflect.Uint64 {
+			fmt.Printf("get data msg:%d\n", msg)
+		}
+		switch k {
+		case reflect.Float32, reflect.Float64:
+			fmt.Printf("get float msg:%f\n", msg)
+		case reflect.String:
+			fmt.Printf("get string msg:%s\n", msg)
+		case reflect.Bool:
+			fmt.Printf("get bool msg:%t\n", msg)
+		case reflect.Slice:
+			slice, _ := msg.([]interface{})
+			for i, v := range slice {
+				fmt.Printf("slice[%d]: %v ,type of %s\n", i, v, reflect.TypeOf(v))
+			}
+		case reflect.Map:
+			tmap, ok := msg.(map[string]interface{})
+			if !ok {
+				fmt.Printf("bad map struct:%v", tmap)
+			}
+			for r, v := range tmap {
+				fmt.Printf("map[%s]:%v\n", r, v)
+			}
+		case reflect.Struct:
+			//todo
+			continue
+		default:
+			fmt.Printf("Unkown msg:%v\n", msg)
+		}
 	}
 }
 
